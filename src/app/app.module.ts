1f9a4786952f129307f3ab380d1e2f19bfa56f31
EdipRubenBahcecioglu <edip-bahcecioglu@hotmail.de>
2023-01-16 23:59:57 +0100

    first components header and about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
