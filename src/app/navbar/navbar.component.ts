1c194bdaa966fedad6765cbc932b814463a20b64
Edip Ruben Bahcecioglu <edip-bahcecioglu@hotmail.de>
2023-02-18 20:30:05 +0100

    remove navbar by clicking on imprint or data protection

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +10,7 @@ export class NavbarComponent implements OnInit {
 
   showResNavbar = false;
 
-
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
